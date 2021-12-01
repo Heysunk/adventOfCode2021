88227cbd008e38058d573016518b1efb410fc448
Michael Chlebek <michael.chlebek@gmail.com>
2021-12-01 11:13:53 +0100

    Done with day1.2

--- day1/day1.sh
+++ day1/day1.sh
@@ -1,9 +1,21 @@
+
+        # If we reaaally cared we could just remove the oldest and add the newest. Window size is 3 so meh
+        #Could also have an array of length windowSize if we wanteed this to be modular as well
+
+
+
 count=0
 
 while read cur; do
-    if [[ -n "$prev" && "$prev" -lt "$cur" ]]; then
-        let count="$count + 1"
+    if [[ -n $prev && -n $prevPrev && -n $prevPrevPrev ]]; then
+        let windowOld="$prev + $prevPrev + $prevPrevPrev"
+        let windowNew="$prev + $prevPrev + $cur"
+        if [[ "$windowOld" -lt "$windowNew" ]]; then
+            let count="$count + 1"
+        fi
     fi
+    prevPrevPrev="$prevPrev"
+    prevPrev="$prev"
     prev="$cur"
 done
 
